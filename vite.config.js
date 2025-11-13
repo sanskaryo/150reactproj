62184939f0276e7869360872203122551c4c72bf
sanskaryo <sanskartodwal@gmail.com>
2025-11-13 15:26:49 +0530

    added rollupwasm for build error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+ import rollupWasm from "@rollup/wasm-node";
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),rollupWasm()],
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+
+// export default defineConfig({
+//   plugins: [react(), rollupWasm()],
+// })
