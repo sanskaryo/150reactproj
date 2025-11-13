491770ea2be97c6a1d19b9a3fedce2f443efe31b
sanskaryo <sanskartodwal@gmail.com>
2025-11-13 15:31:24 +0530

    minor

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- import rollupWasm from "@rollup/wasm-node";
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),rollupWasm()],
+  plugins: [react(),tailwindcss()],
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-//
-
-// export default defineConfig({
-//   plugins: [react(), rollupWasm()],
-// })
